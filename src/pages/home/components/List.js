601e7210db89f2ab6581a2edbf565c297c0d0ba5
bob-bober <nigel.ykaya@gmail.com>
2026-01-01 11:25:48 +0100

    updates list.js

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,13 +1,63 @@
+import { useState } from "react";
 import { useCntxt } from "../../../Context";
 
 export default function List() {
     const {filmList, setFilmList} = useCntxt();
+    const [filterGenre, setFilterGenre] = useState("");
+    
+    const getArray = () => {
+        if (filterGenre === "Action") {
+            <li></li>
+        } else if (filterGenre === "Crime") {
+
+        } else if (filterGenre === "Mistery") {
+
+        } else if (filterGenre === "Comedy") {
+
+        } else if (filterGenre === "Kids") {
+
+        } else {
+
+        };
+    };
+
     
     return (
         <div>
-            <ul>
-                {filmList.map(film => <li key={film.id}>{film.title}</li>)}
-            </ul>
+            <form>
+                <fieldset>
+                    <div>
+                        <label>Filter Genre</label>
+                        <select value={filterGenre} onChange={(e) => setFilterGenre(e.target.value)}>
+                            <option value="" disabled selected>Pick one</option>
+                            <option value="Action">Action</option>
+                            <option value="Crime">Crime</option>
+                            <option value="Mistery">Mistery</option>
+                            <option value="Comedy">Comedy</option>
+                            <option value="Kids">Kids</option>
+                        </select>
+                    </div>
+                </fieldset>
+            </form>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Genre</th>
+                        <th scope="col">Rating</th>
+                        <th scope="col">Watch again</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Grinch</td>
+                        <td>Kids</td>
+                        <td>5</td>
+                        <td>Yes</td>
+                    </tr>
+                </tbody>
+                {getArray()}
+            </table>
         </div>
     );
 };
